59bd4ab9ea8a403ad7f8400795f17c057dc14ce5
aaveshmistry <48035013+aaveshmistry@users.noreply.github.com>
2019-08-05 11:51:23 +1000

    Joystick Movement Update

---
+++ Assets/Scritps/Joysticks/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int moveSpeed = 4;
+    public Joystick joystick;
+  
+    
+    void Start()
+    {
+        this.transform.Translate = new Vector3(joystick.Horizontal, joystick.Vertical * moveSpeed, 0f) * 5f * Time.deltaTime, Space.World;
+    }
+
+    
+}
